b3c6437c7e0042eef2507b506150ff80f7d1236b
amir hossein <amohammadi161183@gmail.com>
2024-09-05 21:00:20 +0330

    lookp field

--- Jango/Jango/settings.py
+++ Jango/Jango/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_*c6#o34v#$(n)x5z7ptlkfl%)$yzf0$&8=6ehjo62e(k3t6z6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
