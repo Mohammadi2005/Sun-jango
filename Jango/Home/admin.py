35b899f0b056a53d8d4e4a979e766cc06ee2f630
amir hossein <amohammadi161183@gmail.com>
2024-10-04 13:52:27 +0330

    actions

--- Jango/Home/admin.py
+++ Jango/Home/admin.py
@@ -1,6 +1,18 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Article, Person
+
 # Register your models here.
+
+@admin.action(description="نمایش دادن مقالات انتخاب شده")
+def make_article_published(self, request, queryset):
+     queryset.update(is_show=True)
+     self.message_user(request, "مقالات انتخاب شده با موفقیت به نمایش در امدند", messages.SUCCESS)
+
+@admin.action(description="پنهان کردن مقالات انتخاب شده")
+def make_article_unpublished(self, request, queryset):
+    queryset.update(is_show=False)
+    self.message_user(request, "مقالات انتخاب شده با موفقیت پنهان شدند", messages.SUCCESS)
+
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'auther', 'pub_date', 'is_show')
@@ -8,6 +20,7 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     list_editable = ['is_show']
     ordering = ['title']
+    actions = [make_article_published, make_article_unpublished]
     fieldsets = [
         [
             'Article info',
@@ -23,7 +36,7 @@ class ArticleAdmin(admin.ModelAdmin):
 
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email')
+    list_display = ('first_name', 'last_name', 'email', 'age')
     search_fields = ('first_name', 'last_name')
 
 admin.site.register(Article, ArticleAdmin)
--- Jango/Home/models.py
+++ Jango/Home/models.py
@@ -4,22 +4,26 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=100) # , null=True, blank=True
-    text = models.TextField(help_text="Enter your article text.")
-    pub_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    is_show = models.BooleanField(default=True)
-    image = models.ImageField(upload_to="Home/image_article/")
-    auther = models.ForeignKey(User, on_delete=models.CASCADE) # این باعث میشه که فقط فردی از بین کاربران ثبت نام شده در این سایت رو بشه به عنوان نویسنده انتخاب کرد
+    title = models.CharField(max_length=100,verbose_name = "عنوان مقاله") # , null=True, blank=True
+    text = models.TextField(help_text="Enter your article text.", verbose_name="متن")
+    pub_date = models.DateTimeField(auto_now_add=True,verbose_name ="تاریخ انتشار")
+    updated_date = models.DateTimeField(auto_now=True ,verbose_name ="تاریخ بروز رسانی")
+    is_show = models.BooleanField(default=True,verbose_name = "ایا مقاله نمایش داده شود")
+    image = models.ImageField(upload_to="Home/image_article/",verbose_name ="تصویر")
+    auther = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name ="نویسنده") # این باعث میشه که فقط فردی از بین کاربران ثبت نام شده در این سایت رو بشه به عنوان نویسنده انتخاب کرد
 # "Person", on_delete=models.CASCADE یععنی برو به مدل person و اگر هم person حذف شد مقالاتش هم حذف بشه
     def __str__(self): # هر وقت کلاس فراخوانی بشه تایتلش برگردونده میشه که توی ادمین پنل این اتفاق میوفته و تایتل هر مقاله نمایش داده میشه
         return self.title
 
+    class Meta:
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقالات"
+
 class Person(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email = models.EmailField()
-    age = models.IntegerField(default=0)
+    first_name = models.CharField(max_length=200,verbose_name ="نام")
+    last_name = models.CharField(max_length=200,verbose_name ="نام خانوادگی")
+    email = models.EmailField(verbose_name ="ایمیل")
+    age = models.IntegerField(default=0,verbose_name = "سن")
     website_url = models.URLField(max_length=200)
     score = models.FloatField()
     score_2 = models.DecimalField(max_digits=5, decimal_places=2, default=0)
@@ -28,3 +32,8 @@ class Person(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.id} "
+
+    class Meta:
+        ordering = ['age']
+        verbose_name = "شخص"
+        verbose_name_plural = "اشخاص"
